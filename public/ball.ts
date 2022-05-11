9c9da41ce50cde1d1866767ccd27cdf35ea571c4
som-syom <dhyeon@student.42seoul.kr>
2022-05-12 00:12:16 +0900

    feat: Point interface 추가, 적용, 공 시작위치 랜덤 범위 수정

--- public/ball.ts
+++ public/ball.ts
@@ -1,51 +1,56 @@
-import { WIDTH, HEIGHT } from "./utils.js";
+import { WIDTH, HEIGHT, Point } from "./utils.js";
 
 export class Ball {
   private readonly radius: number;
-  private x: number;
-  private y: number;
-  private vx: number;
-  private vy: number;
-  private minX!: number;
-  private minY!: number;
-  private maxX!: number;
-  private maxY!: number;
+  private pos: Point = { x: 0, y: 0 };
+  private dir: Point = { x: 0, y: 0 };
+  private min: Point = { x: 0, y: 0 };
+  private max: Point = { x: 0, y: 0 };
 
   constructor(radius: number, speed: number) {
     this.radius = radius;
-    this.vx = speed;
-    this.vy = speed;
+    this.dir.x = speed;
+    this.dir.y = speed;
 
     const diameter = radius * 2;
-    this.x = Math.floor(Math.random() * (WIDTH - diameter * 2)) + diameter * 2;
-    this.y = Math.floor(Math.random() * (HEIGHT - diameter * 2)) + diameter * 2;
+    this.pos.x = Math.floor(Math.random() * (WIDTH - diameter * 2)) + diameter;
+    this.pos.y = Math.floor(Math.random() * (HEIGHT - diameter * 2)) + diameter;
+    console.log(this.pos);
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    this.x += this.vx;
-    this.y += this.vy;
+    this.pos.x += this.dir.x;
+    this.pos.y += this.dir.y;
 
     ctx.fillStyle = "#000000";
     this.bounce(WIDTH, HEIGHT);
 
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
   }
 
   bounce(width: number, height: number) {
-    this.minX = this.radius;
-    this.minY = this.radius;
-    this.maxX = width - this.radius;
-    this.maxY = height - this.radius;
+    this.min.x = this.radius;
+    this.min.y = this.radius;
+    this.max.x = width - this.radius;
+    this.max.y = height - this.radius;
 
-    if (this.x < this.minX || this.x > this.maxX) {
-      this.vx *= -1;
-      this.x += this.vx;
-    } else if (this.y < this.minY || this.y > this.maxY) {
-      this.vy *= -1;
-      this.y += this.vy;
+    if (this.pos.x < this.min.x || this.pos.x > this.max.x) {
+      this.dir.x *= -1;
+      this.pos.x += this.dir.x;
+    } else if (this.pos.y < this.min.y || this.pos.y > this.max.y) {
+      this.dir.y *= -1;
+      this.pos.y += this.dir.y;
     }
+  }
+
+  getPos() {
+    return this.pos;
+  }
+
+  getRadius() {
+    return this.radius;
   }
 }
 
--- public/utils.ts
+++ public/utils.ts
@@ -1,2 +1,7 @@
 export const WIDTH = 1000;
 export const HEIGHT = 500;
+
+export interface Point {
+  x: number;
+  y: number;
+}
